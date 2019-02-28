42b53c14e6f2e56b410d0b33fd4d87aee7da5f0a
ShiraRotman <shiranetmail@gmail.com>
2019-02-28 11:38:57 +0200

    Adding default security.

--- server/rotman/shira/cropx/ElectionsConfig.java
+++ server/rotman/shira/cropx/ElectionsConfig.java
@@ -1,7 +1,16 @@
 package rotman.shira.cropx;
 
+import java.util.Arrays;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -16,4 +25,23 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
     @Bean public WebMvcConfigurer corsConfigurer()
     { return new AppMVCConfigurer(); }
+}
+
+@Configuration @EnableWebSecurity
+class AppSecurityConfigurer extends WebSecurityConfigurerAdapter
+{
+    @Override protected void configure(HttpSecurity security) throws Exception
+    { security.cors(); }
+
+    @Bean public CorsConfigurationSource corsConfigurationSource()
+    {
+        CorsConfiguration config=new CorsConfiguration();
+        config.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        config.setAllowedMethods(Arrays.asList("HEAD","GET","POST"));
+        config.setAllowedHeaders(Arrays.asList("Authorization","Content-Type"));
+        config.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source=new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**",config);
+        return source;
+    }
 }
